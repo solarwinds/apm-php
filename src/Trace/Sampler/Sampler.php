1c4810e7056388bc1f80876d65ef2c9318729374
Jerry Leung <tingfung.leung@solarwinds.com>
2025-08-20 16:10:16 -0700

    Try attach

--- src/Trace/Sampler/Sampler.php
+++ src/Trace/Sampler/Sampler.php
@@ -275,7 +275,7 @@ abstract class Sampler extends OboeSampler
         $this->logDebug('setResponseHeaders: ' . $headers->XTraceOptionsResponse);
         $xTraceOptionsResponseBaggageBuilder = ResponseBaggage::getBuilder();
         $xTraceOptionsResponseBaggage = $xTraceOptionsResponseBaggageBuilder->set('x-trace-options-response', $headers->XTraceOptionsResponse)->build();
-        $xTraceOptionsResponseBaggage->storeInContext(Context::getCurrent())->activate();
+        Context::storage()->attach($xTraceOptionsResponseBaggage->storeInContext(Context::getCurrent()));
         // Context::getCurrent()->withContextValue()
 
         return null;
