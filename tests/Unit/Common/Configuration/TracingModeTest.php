fa4fc9fdcdd1142077e7e8b94fe0f423edbd4b6e
Jerry Leung <tingfung.leung@solarwinds.com>
2025-09-23 18:15:00 -0700

    Added tests for coverage

--- tests/Unit/Common/Configuration/TracingModeTest.php
+++ tests/Unit/Common/Configuration/TracingModeTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Solarwinds\ApmPhp\Tests\Unit\Configuration;
+namespace Solarwinds\ApmPhp\Tests\Unit\Common\Configuration;
 
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
