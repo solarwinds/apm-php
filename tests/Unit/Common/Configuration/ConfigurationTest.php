fbf79dc37f9c5993c22fea484858da41edd8b8ad
Jerry Leung <jerrytfleung@users.noreply.github.com>
2025-09-26 20:33:44 -0700

    Added more test coverage for Configuration class (#58)
    * Added more test coverage for Configuration class
    
    * nits

--- tests/Unit/Common/Configuration/ConfigurationTest.php
+++ tests/Unit/Common/Configuration/ConfigurationTest.php
@@ -49,4 +49,52 @@ class ConfigurationTest extends TestCase
         $config->setEnabled(false);
         $this->assertFalse($config->getEnabled());
     }
+
+    public function test_setters_and_getters()
+    {
+        $config = new Configuration(false, '', '', [], null, false, null, []);
+        $config->setEnabled(true);
+        $this->assertTrue($config->getEnabled());
+        $config->setService('svc');
+        $this->assertEquals('svc', $config->getService());
+        $config->setCollector('coll');
+        $this->assertEquals('coll', $config->getCollector());
+        $config->setHeaders(['foo' => 'bar']);
+        $this->assertEquals(['foo' => 'bar'], $config->getHeaders());
+        $config->setTracingMode(true);
+        $this->assertTrue($config->getTracingMode());
+        $config->setTracingMode(null);
+        $this->assertNull($config->getTracingMode());
+        $config->setTriggerTraceEnabled(true);
+        $this->assertTrue($config->isTriggerTraceEnabled());
+        $closure = function () { return 'abc'; };
+        $config->setTransactionName($closure);
+        $this->assertSame($closure, $config->getTransactionName());
+        $config->setTransactionName(null);
+        $this->assertNull($config->getTransactionName());
+        $config->setTransactionSettings(['x' => 1]);
+        $this->assertEquals(['x' => 1], $config->getTransactionSettings());
+    }
+
+    public function test_to_string_method()
+    {
+        $closure = function () { return 'txn'; };
+        $config = new Configuration(true, 'svc', 'coll', ['h' => 'v'], false, true, $closure, ['s' => 'v']);
+        $str = (string) $config;
+        $this->assertStringContainsString('Configuration(enabled=true, service=svc, collector=coll', $str);
+        $this->assertStringContainsString('headers={"h":"v"}', $str);
+        $this->assertStringContainsString('tracingMode=false', $str);
+        $this->assertStringContainsString('triggerTraceEnabled=true', $str);
+        $this->assertStringContainsString('transactionName=Closure', $str);
+        $this->assertStringContainsString('transactionSettings={"s":"v"}', $str);
+    }
+
+    public function test_to_string_with_nulls()
+    {
+        $config = new Configuration(false, '', '', [], null, false, null, []);
+        $str = (string) $config;
+        $this->assertStringContainsString('enabled=false', $str);
+        $this->assertStringContainsString('tracingMode=null', $str);
+        $this->assertStringContainsString('transactionName=null', $str);
+    }
 }
