6f7ab4fdcc80eb6ca1d912f55f94560b84663c7c
Jerry Leung <tingfung.leung@solarwinds.com>
2025-11-06 11:27:02 -0800

    Fixed GHA

--- tests/Unit/Resource/Detectors/SwoTest.php
+++ tests/Unit/Resource/Detectors/SwoTest.php
@@ -33,19 +33,20 @@ class SwoTest extends TestCase
     public function test_swo_get_resource_service_name_from_sw_apm_service_key(): void
     {
         $serviceKey = getenv(SolarwindsEnv::SW_APM_SERVICE_KEY);
-        if (empty($serviceKey) || !str_contains($serviceKey, ':')) {
+        if ($serviceKey && str_contains($serviceKey, ':')) {
+            [, $service] = explode(':', $serviceKey);
+            $resourceDetector = new swo();
+            $resource = $resourceDetector->getResource();
+            $name = 'solarwinds/apm';
+            $version = InstalledVersions::getPrettyVersion($name);
+
+            $this->assertSame(ResourceAttributes::SCHEMA_URL, $resource->getSchemaUrl());
+            $this->assertSame('apm', $resource->getAttributes()->get(Swo::SW_DATA_MODULE));
+            $this->assertSame($version, $resource->getAttributes()->get(Swo::SW_APM_VERSION));
+            $this->assertSame($service, $resource->getAttributes()->get(ResourceAttributes::SERVICE_NAME));
+        } else {
             $this->markTestSkipped('SW_APM_SERVICE_KEY environment variable is not set or invalid.');
         }
-        [, $service] = explode(':', $serviceKey);
-        $resourceDetector = new swo();
-        $resource = $resourceDetector->getResource();
-        $name = 'solarwinds/apm';
-        $version = InstalledVersions::getPrettyVersion($name);
-
-        $this->assertSame(ResourceAttributes::SCHEMA_URL, $resource->getSchemaUrl());
-        $this->assertSame('apm', $resource->getAttributes()->get(Swo::SW_DATA_MODULE));
-        $this->assertSame($version, $resource->getAttributes()->get(Swo::SW_APM_VERSION));
-        $this->assertSame($service, $resource->getAttributes()->get(ResourceAttributes::SERVICE_NAME));
     }
 
     public function test_swo_detector(): void
