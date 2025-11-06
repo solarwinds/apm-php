eb96b3ad8f499b4e771e0a282cc532200d3106f4
Jerry Leung <tingfung.leung@solarwinds.com>
2025-11-05 16:11:56 -0800

    Fixed sw.apm.version

--- src/Resource/Detectors/Swo.php
+++ src/Resource/Detectors/Swo.php
@@ -12,13 +12,14 @@ use OpenTelemetry\SemConv\ResourceAttributes;
 
 final class Swo implements ResourceDetectorInterface
 {
+    private const PACKAGIST_COMPOSER_NAME = 'solarwinds/apm';
     public const SW_DATA_MODULE = 'sw.data.module';
     public const SW_APM_VERSION = 'sw.apm.version';
     public function getResource(): ResourceInfo
     {
         $attributes = [
             self::SW_DATA_MODULE => 'apm',
-            self::SW_APM_VERSION => InstalledVersions::getRootPackage()['pretty_version'],
+            self::SW_APM_VERSION => InstalledVersions::getPrettyVersion(self::PACKAGIST_COMPOSER_NAME) ?? 'unknown',
         ];
 
         return ResourceInfo::create(Attributes::create($attributes), ResourceAttributes::SCHEMA_URL);
