44281be35cfd9f3628938572e637561406f201b3
Jerry Leung <tingfung.leung@solarwinds.com>
2026-02-02 13:10:20 -0800

    Updated

--- src/Trace/Sampler/HttpSampler.php
+++ src/Trace/Sampler/HttpSampler.php
@@ -92,25 +92,19 @@ class HttpSampler extends Sampler
                 // $cached = "{\"value\":1000000,\"flags\":\"SAMPLE_START,SAMPLE_THROUGH_ALWAYS,SAMPLE_BUCKET_ENABLED,TRIGGER_TRACE\",\"timestamp\":1770065186,\"ttl\":120,\"arguments\":{\"BucketCapacity\":2,\"BucketRate\":1,\"TriggerRelaxedBucketCapacity\":20,\"TriggerRelaxedBucketRate\":1,\"TriggerStrictBucketCapacity\":6,\"TriggerStrictBucketRate\":0.1,\"SignatureKey\":\"a9012f2c6b25d1f5d8b87ed1a3858abd230cac7c99e8ec2aeacfaba6aa31ffc0\"}}";
                 if ($cached !== false) {
                     $unparsed = json_decode($cached, true);
-                    $this->logDebug('unparsed' . implode(',', $unparsed));
-                    if (isset($unparsed['timestamp'], $unparsed['ttl'])) {
-                        $this->logDebug('unparsed timestamp: ' . $unparsed['timestamp']);
-                        $this->logDebug('unparsed ttl: ' . $unparsed['ttl']);
-                    } else {
-                        $this->logDebug('timestamp, ttl is not set');
-                    }
-                    if (is_array($unparsed)) {
-                        $this->logDebug('$unparsed is an array');
-                    } else {
-                        $this->logDebug('$unparsed is not an array');
-                    }
-//                    if (
-//                        isset($unparsed['value'], $unparsed['timestamp'], $unparsed['ttl']) &&
-//                        is_numeric($unparsed['value']) &&
-//                        is_numeric($unparsed['timestamp']) &&
-//                        is_numeric($unparsed['ttl'])
-//                    ) {
-                    if (is_array($unparsed) && (int) $unparsed['timestamp'] + (int) $unparsed['ttl'] <= time()) {
+//                    $this->logDebug('unparsed' . implode(',', $unparsed));
+//                    if (isset($unparsed['timestamp'], $unparsed['ttl'])) {
+//                        $this->logDebug('unparsed timestamp: ' . $unparsed['timestamp']);
+//                        $this->logDebug('unparsed ttl: ' . $unparsed['ttl']);
+//                    } else {
+//                        $this->logDebug('timestamp, ttl is not set');
+//                    }
+//                    if (is_array($unparsed)) {
+//                        $this->logDebug('$unparsed is an array');
+//                    } else {
+//                        $this->logDebug('$unparsed is not an array');
+//                    }
+                    if (is_array($unparsed) && time() <= (int) $unparsed['timestamp'] + (int) $unparsed['ttl']) {
                         $parsed = $this->parsedAndUpdateSettings($unparsed);
                         if ($parsed) {
                             // return if settings are valid
