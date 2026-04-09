be4f67bcf6b923898c0cb33313903d10db5ee57e
Jerry Leung <tingfung.leung@solarwinds.com>
2026-04-09 10:42:07 -0700

    Fixed style's complaint

--- src/Trace/Sampler/OboeSampler.php
+++ src/Trace/Sampler/OboeSampler.php
@@ -346,7 +346,7 @@ abstract class OboeSampler implements SamplerInterface
             // Update bucket from settings
             foreach ($this->buckets as $type => $bucket) {
                 $bucketSettings = $this->settings->buckets[$type] ?? null;
-                if ($settings->timestamp > (int)($bucket->getLastUsed() ?? 0)) {
+                if ($settings->timestamp > (int) ($bucket->getLastUsed() ?? 0)) {
                     if ($bucketSettings !== null && is_a($bucketSettings, BucketSettings::class)) {
                         $bucket->update($bucketSettings->getCapacity(), $bucketSettings->getRate());
                         $this->logDebug('Settings is more recent; update for bucket type ' . $type);
