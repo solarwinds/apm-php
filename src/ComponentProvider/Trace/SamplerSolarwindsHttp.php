f680b5a308585d1e2c29c719a73d13808208919f
Jerry Leung <tingfung.leung@solarwinds.com>
2026-09-15 12:33:02 -0700

    updated

--- src/ComponentProvider/Trace/SamplerSolarwindsHttp.php
+++ src/ComponentProvider/Trace/SamplerSolarwindsHttp.php
@@ -44,7 +44,7 @@ final class SamplerSolarwindsHttp implements ComponentProvider
         $service_name = $arr[1];
         $list = $context->getExtension(ResourceInfo::class);
         $service_name_from_resource = $list->getAttributes()->get('service.name');
-        $config = new Configuration(service:$service_name_from_resource??$service_name??'unknown_service:php', collector:$properties['collector'], token:$token, tracingMode: $properties['tracing_mode'], triggerTraceEnabled: $properties['trigger_tracing_enabled'], transactionSettings: $properties['transaction_settings']);
+        $config = new Configuration(service:$service_name_from_resource??$service_name??'unknown_service:php', collector:'https://' . $properties['collector'], token:$token, tracingMode: $properties['tracing_mode'], triggerTraceEnabled: $properties['trigger_tracing_enabled'], transactionSettings: $properties['transaction_settings']);
 
         return new HttpSampler($context->meterProvider, $config);
     }
